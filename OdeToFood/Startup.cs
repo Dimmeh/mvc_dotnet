3d13affbb29c8c8e2874dcf7bde3334b44cd25cb
Dimmy Marcelissen <dimwes@hotmail.com>
2018-10-01 11:55:22 +0200

    Tutorial ASP.NET Core fundaments
    Getting start

---
+++ OdeToFood/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace OdeToFood
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        // Register services
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IGreeter, Greeter>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, 
+                              IHostingEnvironment env,
+                              IGreeter greeter, 
+                              ILogger<Startup> logger)
+        {
+            if (env.IsDevelopment())
+            {
+                // Geeft een exception dat voor de developers is als er iets fout gaat.
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseFileServer();
+
+            app.UseWelcomePage( new WelcomePageOptions
+            {
+                Path="/wp"
+            });
+
+            //Callback page
+            app.Run(async (context) =>
+            {
+                var greeting = greeter.GetMessageOfTheDay();
+                await context.Response.WriteAsync(greeting);
+            });
+        }
+    }
+}
